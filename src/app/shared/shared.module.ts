70af80b6493072a3b81687c353b456babf5cbe2b
Frederick Fermin <frederickjrfermin123@gmail.com>
2023-12-03 19:20:41 -0600

    Implement logOut

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
+import { PagesRoutingModule } from '../Pages/pages-routing.module';
 
 
 
@@ -11,7 +12,8 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    PagesRoutingModule
   ],
   exports: [
     HeaderComponent
